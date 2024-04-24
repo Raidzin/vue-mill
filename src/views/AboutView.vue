662fe85348b1095fe133b81267a667f2ae55b699
Raidzin <60663550+Raidzin@users.noreply.github.com>
2024-04-25 01:10:49 +0300

    add github login button

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="about">
-    <h1>Ну короче тут будет фронтенд для игры <a href="https://github.com/Raidzin/MillServer">Мельница</a></h1>
+    <h1>
+      Ну короче тут будет фронтенд для игры
+      <a href="https://github.com/Raidzin/MillServer">Мельница</a>
+      <a href="https://raidzin.ddns.net:30000/api/v1/oauth/github/login">Зайти через <strong>GitHub</strong></a>
+    </h1>
   </div>
 </template>
 
