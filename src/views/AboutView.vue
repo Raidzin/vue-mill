35accd8f4f5fb657becbc4820210f97c54fd6241
Raidzin <60663550+Raidzin@users.noreply.github.com>
2024-04-25 01:20:34 +0300

    change about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="about">
     <h1>
-      Ну короче тут будет фронтенд для игры
-      <a href="https://github.com/Raidzin/MillServer">Мельница</a>
-      <a href="https://raidzin.ddns.net:30000/api/v1/oauth/github/login">Зайти через <strong>GitHub</strong></a>
+      Зайти через
+      <a href="https://raidzin.ddns.net:30000/api/dev/oauth/github/login">GitHub</a>
     </h1>
   </div>
 </template>
